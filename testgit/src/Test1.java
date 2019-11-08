d34d5fa0f389a208857d001dde2501466119d64d
lmqkongling <527573251@qq.com>
2019-11-08 17:14:27 +0800

    11

--- testgit/src/Test1.java
+++ testgit/src/Test1.java
@@ -1,8 +1,7 @@
 public class Test1 {
    public static void main(String[] args) {
-       System.out.println("新建分支为2019110901");
+       System.out.println("master分支");
        System.out.println("aaa");
-
-
+       System.out.println("新建分支为2019110901");
    }
 }
