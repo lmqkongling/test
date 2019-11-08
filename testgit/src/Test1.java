37906a9001b335ef9e6462f4db46b155c187dc25
lmqkongling <57479058+lmqkongling@users.noreply.github.com>
2019-11-08 09:55:26 +0800

    Update Test1.java

--- testgit/src/Test1.java
+++ testgit/src/Test1.java
@@ -1,6 +1,7 @@
 public class Test1 {
    public static void main(String[] args) {
         System.out.println("2222222222222333333333");
+        System.out.println("2222222222222333333333");
       
     }
 }
