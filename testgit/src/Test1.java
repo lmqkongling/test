3d912d6118a4241ccb3f4cb1335d9d2607407e66
lmqkongling <527573251@qq.com>
2019-11-08 14:08:23 +0800

    add java file

--- testgit/src/Test1.java
+++ testgit/src/Test1.java
@@ -1,7 +1,9 @@
 public class Test1 {
    public static void main(String[] args) {
-        System.out.println("2222222222222333333333");
-        System.out.println("2222222222222333333333");
-      
-    }
+       System.out.println("2222222222222333333333");
+       System.out.println("2222222222222333333333");
+       System.out.println("aaa");
+
+
+   }
 }
