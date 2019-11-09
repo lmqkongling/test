a969efc9b206025521eeed470adb3572590afa56
lmqkongling <57479058+lmqkongling@users.noreply.github.com>
2019-11-09 09:42:07 +0800

    Update Test1.java

--- testgit/src/Test1.java
+++ testgit/src/Test1.java
@@ -3,5 +3,6 @@ public class Test1 {
        System.out.println("master分支");
        System.out.println("aaa");
        System.out.println("新建分支为2019110901");
+       System.out.println("新增一行，当前时间为20191109 0942");
    }
 }
